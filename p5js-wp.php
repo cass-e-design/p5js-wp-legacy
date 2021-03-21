9112591c43c69f301d5ebde25f90b738773cc313
Cass-E Design <cass@cass-e.net>
2021-03-21 14:14:55 -0400

    Initial commit

---
+++ p5js-wp.php
@@ -0,0 +1,89 @@
+<?php
+/*
+Plugin Name: p5js wp
+Plugin URI: https://github.com/cass-e-design/p5js-wp-legacy
+Description: Local hosting & embedding of p5.js scripts directly in posts & pages using iframes & shortcode
+Version: 1.0.0
+Date: 21/03/2018
+Author: Cassia Duske
+Author URI: http://cass-e.net
+License: GPL2
+
+Core functionality
+
+Copyright (C) 2021 Cassia Duske
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+function p5jswp_styles() {
+	wp_enqueue_style('pjswp-style', plugins_url('css/style.css', __FILE__));
+}
+add_action('wp_enqueue_scripts', 'p5jswp_styles');
+
+function p5jswp_process_shortcode($attrs, $content) {
+
+	$script_itself = '';
+	if (isset($attrs['js'])) {
+		$script_itself = '<script>' . $attrs['js'] . '</script>';
+	} else if (isset($attrs['script'])) {
+		$script_itself = '<script src="' . $attrs['script'] . '"></script>';
+	} else {
+		$script_itself = '<p>' . __('p5jswp: WARNING: No `script` or `js` attributes specified', 'p5js-wp') . '</p>';
+	}
+
+	$css = '';
+	if (isset($attrs['css'])) $css = '<style>'.$attrs['css'].'</style>';
+	
+	$width = '';
+	if (isset($attrs['width'])) $width = $attrs['width'];
+	$height = '';
+	if (isset($attrs['height'])) $width = $attrs['height'];
+
+
+	$libraries = '';
+	if (isset($attrs['libraries']))
+		foreach (explode(' ', $attrs['libraries']) as $library)
+			$libraries .= '<script src="'.$library.'"></script>';
+
+	//default
+	$libraries .= '<script src="'.plugins_url('/js/p5.min.js', __FILE__).'"></script>';
+	
+	//htmlspecialchars prevents contents of the scripts from interfering with the iframe
+	$iframe_interior = htmlspecialchars('<html>
+				<head>'.$libraries.$css.'</head>
+				<body>
+					'.$script_itself.'
+				</body>
+			</html>');
+
+	$output = '<figure class="wp-block-image">
+		<iframe class="p5jswp" '.$width.' '.$height.'sandbox="allow-scripts allow-pointer-lock allow-same-origin allow-popups allow-forms allow-modals" srcdoc="'.$iframe_interior.'"></iframe>';
+	
+		if (isset($attrs['caption'])) {
+		$output .= '<figcaption>' . $attrs['caption'] . '</figcaption>';
+	}
+
+	$output .= '</figure>';
+	return $output;
+}
+add_shortcode('p5jswp', 'p5jswp_process_shortcode');
+
+//Move the smartcodes later on in the loading so that they don't mess with scripts
+remove_filter('the_content', 'wptexturize');
+add_filter('the_content', 'wptexturize', 99);		
+
+?>
---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== p5.js WP ===
+Contributors: cass-e
+Donate link: http://cass-e.net
+Tags: p5js, embed, iframe, javascript
+Requires at least: 4.0
+Tested up to: 5.7
+Requires PHP: 4.3
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ 
+Local hosting & embedding of p5.js scripts directly in posts & pages using iframes & shortcode
+ 
+== Description ==
+
+Local hosting & embedding of p5.js scripts directly in posts & pages using iframes & shortcode [p5jswp]
+Attributes include:
+* **script**: hardcoded URL to a js file
+* **js**: literal javascript instead of an external file\*
+* **css**: literal css included inside iframe
+* **width**: a width for the iframe. Remember to include units!
+* **height**: a height for the iframe. Remember to include units!
+* **caption**: Specify a caption inside the figure
+* **libraries**: space delimited list of hardcoded URLs to libraries to be included in the iframe's `<head>`
+
+\*I've had to encode the js with [htmlspecialchars()](https://www.php.net/manual/en/function.htmlspecialchars.php) to avoid messing with html attribute (quoted string) this is all being included inside of. This could mess with script output, especially if you're modifying literal DOM strings
+
+
+== Installation ==
+ 
+1. Upload `p5js-wp` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+ 
+== Frequently Asked Questions ==
+ 
+= What are the differences between this and *the other version I may someday release* =
+ 
+I think this version should have very widespread compatibility - I don't think I've used any features new to PHP 7 or Wordpress versions newer than 5.
+ 
+== Screenshots ==
+ 
+== Changelog ==
+ 
+= 1.0 =
+* Initial release. Moved to srcdoc attribute; added support for JS content saved in editor, css, etc.
+ 
+= 0.5 =
+* Original version: Passed all content into the iframe via GET request
+ 
+== Upgrade Notice ==
+ 
+= 1.0 =
+Technically drops support for Internet Explorer. Oops.
